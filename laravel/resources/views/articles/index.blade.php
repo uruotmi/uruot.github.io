775d1cb5cfcd20fb4e72a318382c1202b89a8896
uruotmi <102810062+uruotmi@users.noreply.github.com>
2023-01-02 01:13:16 +0900

    Add files via upload

---
+++ laravel/resources/views/articles/index.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('title', '記事一覧')
+
+@section('content')
+  @include('nav')
+  <div class="container">
+    @foreach($articles as $article)
+    @include('articles.card') 
+
+    @endforeach
+  </div>
+@endsection
---
+++ laravel/resources/views/nav.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar navbar-expand navbar-dark blue-gradient">
+
+  <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>Memo</a>
+
+  <ul class="navbar-nav ml-auto">
+
+    @guest {{--この行を追加--}}
+    <li class="nav-item">
+      <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a> {{--この行を変更--}}
+    </li>
+    @endguest {{--この行を追加--}}
+
+    @guest {{--この行を追加--}}
+    <li class="nav-item">
+    <a class="nav-link" href="{{ route('login') }}">ログイン</a> 
+    </li>
+    @endguest {{--この行を追加--}}
+      
+    @auth {{--この行を追加--}}
+    <li class="nav-item">
+    <a class="nav-link" href="{{ route('articles.create') }}"><i class="fas fa-pen mr-1"></i>投稿する</a> 
+    </li>
+    @endauth {{--この行を追加--}}
+    
+    @auth {{--この行を追加--}}
+    <!-- Dropdown -->
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+         aria-haspopup="true" aria-expanded="false">
+        <i class="fas fa-user-circle"></i>
+      </a>
+      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+        <button class="dropdown-item" type="button"
+            onclick="location.href='{{ route("users.show", ["name" => Auth::user()->name]) }}'">
+          マイページ
+        </button>
+        <div class="dropdown-divider"></div>
+        <button form="logout-button" class="dropdown-item" type="submit">
+          ログアウト
+        </button>
+      </div>
+    </li>
+    <form id="logout-button" method="POST" action="{{ route('logout') }}"> {{--この行を編集--}}
+      @csrf {{--この行を追加--}}
+    </form>
+    <!-- Dropdown -->
+    @endauth {{--この行を追加--}}
+
+  </ul>
+
+</nav>
